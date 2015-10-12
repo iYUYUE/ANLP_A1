8d58defd284d07996c9afd0b7dad936bac49b675
ChunchuanLv <chunchuan.lv@gmail.com>
2015-10-12 21:50:03 +0100

    Naive_Trigram
    Without dictionary in dictionary

--- code/asgn1-helper.py
+++ code/asgn1-helper.py
@@ -2,14 +2,15 @@
 from __future__ import division
 import re
 import sys
+import json
 from random import random
 from math import log
 from collections import defaultdict
 
 
 tri_counts=defaultdict(int) #counts of all trigrams in input
-
-
+pairsCounts = defaultdict(int)
+conditionProbs = defaultdict(float)
 #this function currently does nothing.
 def preprocess_line(line):
     #remove non-necessary characters, and turn string to lowercase
@@ -26,7 +27,7 @@ if len(sys.argv) != 2:
     print "Usage: ", sys.argv[0], "<training_file>"
     sys.exit(1)
 
-infile = sys.argv[1] #get input argument: the training file
+infile = '../data/'+sys.argv[1] #get input argument: the training file
 
 #This bit of code gives an example of how you might extract trigram counts
 #from a file, line by line. If you plan to use or modify this code,
@@ -40,14 +41,22 @@ with open(infile) as f:
             trigram = line[j:j+3]
             tri_counts[trigram] += 1
 
+for trigram in tri_counts.keys():
+    pairsCounts[trigram[0:2]] +=  tri_counts[trigram]
+for trigram in tri_counts.keys():
+    conditionProbs[trigram] = tri_counts[trigram]/pairsCounts[trigram[0:2]]
 #Some example code that prints out the counts. For small input files
 #the counts are easy to look at but for larger files you can redirect
 #to an output file (see Lab 1).
-print "Trigram counts in ", infile, ", sorted alphabetically:"
-for trigram in sorted(tri_counts.keys()):
-    print trigram, ": ", tri_counts[trigram]
-print "Trigram counts in ", infile, ", sorted numerically:"
-for tri_count in sorted(tri_counts.items(), key=lambda x:x[1], reverse = True):
-    print tri_count[0], ": ", str(tri_count[1])
-
+#print "Trigram counts in ", infile, ", sorted alphabetically:"
+#for trigram in sorted(tri_counts.keys()):
+#    print trigram, ": ", tri_counts[trigram]
+#print "Trigram counts in ", infile, ", sorted numerically:"
+#for tri_count in sorted(tri_counts.items(), key=lambda x:x[1], reverse = True):
+#    print tri_count[0], ": ", str(tri_count[1])
+print "Conditional probability"
+for conditionP in sorted(conditionProbs.keys()):
+    print 'P('+conditionP[2:3]+'|'+conditionP[0:2]+')=',conditionProbs[conditionP]
+    
+json.dump(conditionProbs, open('../data/model_'+sys.argv[1]+'.txt','w'))
 
