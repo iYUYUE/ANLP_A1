051cbd09434bee3c94441df6867b4aa0f7d2d123
ChunchuanLv <chunchuan.lv@gmail.com>
2015-10-12 23:20:16 +0100

    [[]

--- code/asgn1-helperV2.py
+++ code/asgn1-helperV2.py
@@ -10,6 +10,9 @@ from collections import defaultdict
 
 
 tri_counts=defaultdict(int) #counts of all trigrams in input
+uni_counts=defaultdict(int) #counts of all trigrams in input
+bi_counts=defaultdict(int) #counts of all trigrams in input
+
 pairsCounts = defaultdict(int)
 conditionProbs = collections.defaultdict(dict)
 #this function currently does nothing.
@@ -17,10 +20,11 @@ def preprocess_line(line):
     #remove non-necessary characters, and turn string to lowercase
     p = re.compile('[^\w\s,.]')
     line = re.sub(p,'',line.lower())
+    line = re.sub('\n',']',line)
     #turn numbers into 0
     line = re.sub('[0-9]','0',line)
-    return line
-
+    #add begining and end [[ ]
+    return '[['+line
 
 #here we make sure the user provides a training filename when
 #calling this program, otherwise exit with a usage error.
@@ -38,10 +42,11 @@ infile = '../data/'+sys.argv[1] #get input argument: the training file
 with open(infile) as f:
     for line in f:
         line = preprocess_line(line) #doesn't do anything yet.
-        for j in range(len(line)-(3)):
+        #our model unit is a line instead of sentence
+        for j in range(len(line)-(2)):
             trigram = line[j:j+3]
             tri_counts[trigram] += 1
-
+            
 for trigram in tri_counts.keys():
     pairsCounts[trigram[0:2]] +=  tri_counts[trigram]
 for trigram in tri_counts.keys():
@@ -50,7 +55,15 @@ for trigram in tri_counts.keys():
     else:
         prob = defaultdict(float)
         prob[trigram[2:3]] =  tri_counts[trigram]/pairsCounts[trigram[0:2]]
-        conditionProbs[trigram[0:2]]  = prob
+        conditionProbs[trigram[0:2]]  = prob 
+
+#totalCounts = 0
+#for counts in uni_counts.values():
+#    totalCounts += counts
+#prob = defaultdict(float)
+#for uni in uni_counts.keys():
+#    prob[uni] = uni_counts[uni]/totalCounts
+#conditionProbs['[['] = prob
 #Some example code that prints out the counts. For small input files
 #the counts are easy to look at but for larger files you can redirect
 #to an output file (see Lab 1).
