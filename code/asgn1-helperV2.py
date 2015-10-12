1f344d058be53c63ce2e9ccd91b29104fc51b454
Yue Yu <admin@iyuyue.com>
2015-10-12 23:21:06 +0100

    random output function added

--- code/asgn1-helperV2.py
+++ code/asgn1-helperV2.py
@@ -7,6 +7,7 @@ import json
 from random import random
 from math import log
 from collections import defaultdict
+import numpy as np
 
 
 tri_counts=defaultdict(int) #counts of all trigrams in input
@@ -20,6 +21,37 @@ def preprocess_line(line):
     #turn numbers into 0
     line = re.sub('[0-9]','0',line)
     return line
+
+# this function generate random output from the estimated language models.
+def generate_random_output(distribution, N):
+    ''' generate_random_sequence takes a distribution (represented as a
+    dictionary of outcome-probability pairs) and a number of samples N
+    and returns a list of N samples from the distribution.  
+    This is a modified version of a sequence generator by fraxel on
+    StackOverflow:
+    http://stackoverflow.com/questions/11373192/generating-discrete-random-variables-with-specified-weights-using-scipy-or-numpy
+    '''
+    #As noted elsewhere, the ordering of keys and values accessed from
+    #a dictionary is arbitrary. However we are guaranteed that keys()
+    #and values() will use the *same* ordering, as long as we have not
+    #modified the dictionary in between calling them.
+    output = '';
+    for i in range(1, N):
+        if len(output<2) or (output[-1:] is ']'):
+            i += 2
+            output += '[['
+            continue
+        outcomes = np.array(distribution[output[-2:]].keys())
+        probs = np.array(distribution[output[-2:]].values())
+        #make an array with the cumulative sum of probabilities at each
+        #index (ie prob. mass func)
+        bins = np.cumsum(probs)
+        #create N random #s from 0-1
+        #digitize tells us which bin they fall into.
+        #return the sequence of outcomes associated with that sequence of bins
+        #(we convert it from array back to list first)
+        output += outcomes[np.digitize(random_sample(1)[0], bins)]
+    return output
 
 
 #here we make sure the user provides a training filename when
